68040769d1cdb5e5b3ade989d3aa1ac510b34683
samantha <srmoor6@ilstu.edu>
2020-01-31 21:17:58 -0600

    ss

--- p5/Week2/mp1/sketch.js
+++ p5/Week2/mp1/sketch.js
@@ -1,13 +1,17 @@
 let earth;
+let font;
 
 function preload() {
   earth = loadImage('img/earth1.jpg');
+  font = loadFont('RobotoMono-Bold.ttf');
 }
 
 function setup() {
   // put setup code here
-  createCanvas(800, 800, WEBGL);
+  createCanvas(900, 800, WEBGL);
+  textFont(font);
 
+  textAlign(LEFT, CENTER)
 }
 
 function draw() {
@@ -15,6 +19,11 @@ function draw() {
 
   if (mouseIsPressed) {
       background(51, 51, 77);
+
+  textAlign(RIGHT);
+  textSize(40);
+  text('By night, he escapes his regular household life ', 184, 158);
+  text('and plots to rule the world', 99, 190);
 
   fill(191, 128, 64);
     ellipse(252, 144, 202, 202);
@@ -39,15 +48,13 @@ function draw() {
     ellipse(290, 122, 40, 40);
 
     texture(earth);
-  sphere(240);
-
-    fill(102,102,51);
-    ellipse(380, 520, 520, 520);
-
+  sphere(200, 607);
 
   }
   else {
 background(128, 223, 255);
+
+
 
     fill(191, 128, 64);
     ellipse(247, 343, 120, 95);
@@ -70,6 +77,11 @@ background(128, 223, 255);
     ellipse(138, 327, 10, 10);
     fill('black');
     ellipse(164, 327, 10, 10);
+
+    textAlign(RIGHT);
+    textSize(40);
+    text('This is a Leo the cat', 184, 158);
+    text('a simple pet by day', 99, 190);
   }
 }
 
