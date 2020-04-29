f7d9dc572c5aa41f3b4be8e57ca02f18d33b2c13
samantha <srmoor6@ilstu.edu>
2020-04-28 22:10:40 -0500

    4

---
+++ p5/Week14+15/gyro/sketch.js
@@ -0,0 +1,91 @@
+/* For mobile phones - accesses accelerometer.
+Make sure you turn on orientation lock on your iPhone or Android device. */
+
+var alpha, beta, gamma; // orientation data
+var bugImage;
+var xPosition = 0;
+var yPosition = 0;
+var x = 0; // acceleration data
+var y = 0;
+var z = 0;
+
+function setup() {
+
+  createCanvas(windowWidth, windowHeight);
+
+  // initialize accelerometer variables
+  alpha = 0;
+  beta = 0;
+  gamma = 0;
+
+  bugImage = loadImage("assets/fly.png");
+  imageMode(CENTER);
+  rectMode(CENTER);
+
+}
+
+function draw() {
+
+//  background('#c6f5ff'); // light blue
+
+  // the map command !!!!
+  // takes your variable and maps it from range 1 to range 2
+  // map(yourVar, range1_x, range1_y, range2_x, range2_y) ;
+  xPosition = map(gamma, -60, 60, 0, width);
+  yPosition = map(beta, -30, 30, 0, height);
+
+  push(); // before you use translate, rotate, or scale commands, push and then pop after
+
+  translate(xPosition, yPosition); // move everything over by x, y
+
+  rotate(radians(alpha)); // using alpha in here so it doesn't feel bad
+
+  image(bugImage, 0, 0, 500, 500);
+  //  	rect(0, 0, 100, 100) ;
+  pop();
+
+
+  // DECORATIONS
+  // Just a bunch of text commands to display data coming in from addEventListeners
+  //REMOVE WHEN DONE
+  textAlign(LEFT);
+  textSize(20);
+  fill('black');
+  text("orientation data:", 25, 25);
+  textSize(15);
+  text("alpha: " + alpha, 25, 50);
+  text("beta: " + beta, 25, 70);
+  text("gamma: " + gamma, 25, 90);
+  textSize(20);
+  text("acceleration data:", 25, 125);
+  textSize(15);
+  text("x = " + x.toFixed(2), 25, 150); // .toFixed means just show (x) decimal places
+  text("y = " + y.toFixed(2), 25, 170);
+  text("z = " + z.toFixed(4), 25, 190);
+
+  // MORE DECORATIONS - write that pretty ATK type on top.
+  fill('white');
+  noStroke();
+  textSize(300);
+  textAlign(CENTER);
+  text("you stink", width / 4, height / 4);
+
+}
+
+// HERE'S THE STUFF YOU NEED FOR READING IN DATA!!!
+
+// Read in accelerometer data
+window.addEventListener('deviceorientation', function(e) {
+  alpha = e.alpha;
+  beta = e.beta;
+  gamma = e.gamma;
+});
+
+
+// accelerometer Data
+window.addEventListener('devicemotion', function(e) {
+  // get accelerometer values
+  x = e.acceleration.x;
+  y = e.acceleration.y;
+  z = e.acceleration.z;
+});
---
+++ p5/Week14+15/weather/sketch.js
@@ -0,0 +1,61 @@
+var weather;
+var temp = 0;
+var weatherID = 0; // returned in the JSON weather element
+var myState = 0;
+var x = 0;
+var ws = 0;
+var temper = 0;
+
+function setup() {
+  createCanvas(400, 400);
+
+  // HERE Is the call to get the weather. PLEASE PUT YOUR OWN ID IN HERE!!!
+  // MINE WILL STOP WORKING IF EVERYONE USES IT!!!
+
+  var myJSONString = 'https://api.openweathermap.org/data/2.5/weather?zip=61761,us&units=imperial&';
+  var myIDString = '405a98f3ac41669a7a503f0062876f63'; // Take out the x’s and ADD YOUR ID!!!
+  loadJSON(myJSONString + myIDString, gotData); // look - that gotData function happens when JSON comes back.
+
+}
+
+
+function gotData(data) {
+  weather = data;
+  console.log(data); // for debugging purposes, print out the JSON data when we get it.
+  ws = weather.wind.speed;
+  temper = weath.main.temp;
+}
+
+
+function draw() {
+  switch (myState) {
+    case 0:
+      if (weather) {
+          myState = 1;
+      }
+      break;
+
+    case 1:
+    background(200) ;
+    text("we have weather", 20, 20) ;
+// parse the weather object and put some text or images using at least 3 different weather data!
+fill('black');
+text("the temperature is" + weather.main.temp, 20, 40) ;
+text("the humidty is" + weather.main.humidity, 20, 60) ;
+text("the wind speed is" + weather.wind.speed, 20, 80) ;
+
+fill('white') ;
+noStroke9();
+ellipse(100,300,90,50);
+x = x + ws/2 ;
+if (x > width) x = 0 ;
+
+fill('red');
+var tmp = 0;
+tmp = map(temper, -10, 90, 2, height-10);
+rect(width-40, height-10, 30, -tmp );
+
+      break;
+
+  }
+}
