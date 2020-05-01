a8a3dff9f42b3617d7ba5d3080936d71e07145e0
samantha <srmoor6@ilstu.edu>
2020-04-30 21:25:50 -0500

    aa

---
+++ p5/Week14+15/geolocation/sketch.js
@@ -0,0 +1,30 @@
+var num ;
+var distance ;
+
+var locationData;
+function preload(){
+    locationData =  getCurrentPosition();
+}
+
+function setup(){
+	num = 0 ;
+    intervalCurrentPosition(positionPing, 5000) ;
+	createCanvas(displayWidth, displayHeight) ;
+}
+
+function draw() {
+
+
+}
+function positionPing(position){
+	textSize(24) ;
+	num++ ;
+	background(255) ;
+    text("lat: " + position.latitude, 10, 40);
+    text("long: " + position.longitude, 10, 90);
+	text("number of updates " + num, 10, 140) ;
+
+	distance = calcGeoDistance(locationData.latitude, locationData.longitude, position.latitude, position.longitude, 'mi') ;
+	text("you have moved " + distance, 10, 190) ;
+
+}
